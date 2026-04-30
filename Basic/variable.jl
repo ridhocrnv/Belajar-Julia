2424805394d39e2ff3c0040f2b8b922aed1215a9
Ridho Crnv <ridhoahmadirawan281@gmail.com>
2026-04-30 09:33:28 +0800

    feat: variable.jl

---
+++ Basic/variable.jl
@@ -0,0 +1,88 @@
+# You can use hash sign to define a one line comment in Julia.
+# For multi-line comments, you can use #= and =# to enclose the comment block. Like this following example:
+
+#=
+    Testing multi-line comments in Julia.
+    https://github.com/ridhocrnv/Belajar-Julia.git
+=#
+
+a = 24
+
+println(typeof(a)) # Get the type of a variable, Int64
+
+a = a + 1
+println(a) # Print the value of a, which is now 25
+
+a = 12; b = 24; c = 36 # You can define multiple variables in one line by separating them with semicolons (;).
+
+c = b^2 + 2*b
+println(c) # Print the value of c, which is 624
+
+a = "Hello, Julia!" # Dynamically assign valuesto a variable, a was an Int64 before, but now it is a String.
+println(typeof(a)) # Print the type of a, which is String
+
+d = 3.14
+println(typeof(d)) # Print the type of d, which is Float64
+
+# A variable name cannot start with a number,  but you can use uppercase, lowercase or underscore
+_abc = 456
+println(_abc)
+
+
+# We can use unicode characters for variable or function names in Julia.
+π = 3.14159
+println(π) # Print the value of π, which is 3.14159
+
+γ = 0.57721 # \gamma TAB
+println(γ) # Print the value of γ, which is 0.57721
+
+α = 1.61803 # \alpha TAB
+println(α) # Print the value of α, which is 1.61803
+
+β = 0.69314 # \beta TAB
+println(β) # Print the value of β, which is 0.69314
+
+
+# You can also use subscript and superscript characters in variable names in Julia.
+β₀ = 0.5 # \beta\_0 TAB
+println(β₀) # Print the value of β₀, which is 0.5
+
+γ¹ = 10 # \gamma\^1 TAB
+println(γ¹) # Print the value of γ¹, which is 10
+
+
+# However do not over use unicode characters, they can make your program confusing.
+
+# You cannot use the following keywords as variable names
+
+#=
+    Keywords in Julia:
+    abstract, baremodule, begin, break, catch, const, continue, do, else, elseif,
+    end, export, false, for, function, global, if, import, let, local, macro, module,
+    quote, return, true, try, using, while
+=#
+
+# begin = 12 # throw Error: syntax: unexpected symbol "begin"
+# if = 24 # throw Error: syntax: unexpected symbol "if"
+
+π = 3.14 # This will overwrite the value of π, which is a built-in constant in Julia.
+println(π) # Print the value of π, which is now 3.14
+
+# Naming style cpnventions in Julia for variable names:
+
+first_name = "John" # Use snake_case for variable names, which is the convention in Julia.
+last_name = "Doe"
+full_name = first_name * " " * last_name # Concatenate strings using the * operator
+println(full_name) # Print the full name, which is "John Doe"
+
+firstName = "Jane" # You can also use camelCase for variable names, but it is not the convention in Julia.
+lastName = "Smith"
+fullName = firstName * " " * lastName
+println(fullName) # Print the full name, which is "Jane Smith"
+
+
+p, q, r = 1, 2, 3 # You can also use multiple assignment to assign values to multiple variables in one line.
+println(p, ", ", q, ", ", r) # Print the values of p, q, and r, which are 1, 2, and 3 respectively
+
+p, q = q, p # You can also swap the values of two variables using multiple assignment.
+println(p, ", ", q) # Print the values of p and q, which are now 2 and 1 respectively
